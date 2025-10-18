7cf47f90ff1d3ac8b62a4109b79e09f07ef65af7
creativeplot <victor.gamer1@hotmail.com>
2025-10-17 21:06:36 -0300

    query client set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import LoginPage from "./components/pages/LoginPage";
 import SignupPage from "./components/pages/SignupPage";
 import DashboardPage from "./components/pages/DashboardPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 // Next i will build a dashboard, with a sidebar, header and main content area
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="signup" element={<SignupPage/>}/>
         <Route path="dashboard" element={<DashboardPage/>}/>
       </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
